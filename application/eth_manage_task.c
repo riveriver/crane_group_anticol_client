d0474c5d83d8d9b18d1e4ec5e892b1d5e12098f1
river <hello.river@outlook.com>
2026-05-17 21:02:47 +0800

    refactor: Macro defines bits instead of magic numbers

--- application/eth_manage_task.c
+++ application/eth_manage_task.c
@@ -144,8 +144,8 @@ static void eth_udp_recv(void *arg,
         LOG_E("Wrote load weight percentage to Modbus register\n");
       }
       
-      /* Set data valid bit for load weight (bit 4) */
-      mb_reg_write_bit(REG_DATA_VAILD, 4, 1);
+      /* Set data valid bit for load weight */
+      mb_reg_write_bit(REG_DATA_VAILD, REG_DATA_VAILD_BIT_LOAD_WEIGHT, 1);
       
       LOG_D("Moment:%.3f t*m, MomentPct:%.3f%%, Weight:%.3f t, WeightPct:%.3f%%\n",
             lifting_moment, lifting_moment_pct, load_weight, load_weight_pct);
--- application/read_swing_encoder_task.c
+++ application/read_swing_encoder_task.c
@@ -66,8 +66,8 @@ void read_swing_encoder_thread(void *argument){
             LOG_D("\r\n");
             mb_reg_write_u32(ENCODER_REG_ADDR, telegram.u16reg[0], telegram.u16reg[1]);
             
-            /* Set data valid bit for swing encoder (bit 0) */
-            mb_reg_write_bit(REG_DATA_VAILD, 0, 1);
+            /* Set data valid bit for swing encoder */
+            mb_reg_write_bit(REG_DATA_VAILD, REG_DATA_VAILD_BIT_SWING_ENCODER, 1);
         }
         
     }
