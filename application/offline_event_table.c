e699a5b6d5175950483758db1de06b7296d9ab3c
river <hello.river@outlook.com>
2026-05-18 17:00:19 +0800

    fix: Update logging format and enable offline events in event table

--- application/offline_event_table.c
+++ application/offline_event_table.c
@@ -15,7 +15,7 @@ offline_event offline_reset_event = NO_OFFLINE;
 /* System protection: log and reset */
 void system_protect_offline_first(void)
 {
-    LOG_E("System protect triggered by offline event: %d\r\n", offline_reset_event);
+    LOG_E("System protect triggered by offline event[%d]\r\n", offline_reset_event);
     /* wait to ensure log is flushed before reset */
     // osDelay(1000);
     // __set_FAULTMASK(1);
@@ -24,13 +24,17 @@ void system_protect_offline_first(void)
 
 void system_protect_offline(void)
 {
-    LOG_E("system protect caused by offline event: %d\r\n", offline_reset_event);
+    LOG_D("system protect caused by offline event[%d]\r\n", offline_reset_event);
 }
 
 void read_load_weight_offline_first(void)
 {
     LOG_E("Load weight data offline\r\n");
     mb_reg_write_bit(REG_DATA_VAILD, REG_DATA_VAILD_BIT_LOAD_WEIGHT, false);
+    mb_reg_write_float(REG_LOAD_WEIGHT, 0.0f);
+    mb_reg_write_float(REG_LOAD_WEIGHT_PCT, 0.0f);
+    mb_reg_write_float(REG_LIFTING_MOMENT, 0.0f);
+    mb_reg_write_float(REG_LIFTING_MOMENT_PCT, 0.0f);
 }
 /* ============================================================================
  * PROJECT-SPECIFIC OFFLINE EVENT TABLE
@@ -52,7 +56,7 @@ struct offline_manage_obj offline_event_table[] =
     },
     {
         .event = OFFLINE_ETH_MANAGE,
-        .enable = DISABLE,
+        .enable = ENABLE,
         .error_level = OFFLINE_ERROR_LEVEL,
         .group_id = 3,
         .offline_time = 1000,
@@ -63,7 +67,7 @@ struct offline_manage_obj offline_event_table[] =
     },
     {
         .event = OFFLINE_READ_SWING_ENCODER,
-        .enable = DISABLE,
+        .enable = ENABLE,
         .error_level = OFFLINE_ERROR_LEVEL,
         .group_id = 2,
         .offline_time = 1000,
@@ -74,7 +78,7 @@ struct offline_manage_obj offline_event_table[] =
     },
     {
         .event = OFFLINE_READ_LUFFING_ENCODER,
-        .enable = DISABLE,
+        .enable = ENABLE,
         .error_level = OFFLINE_ERROR_LEVEL,
         .group_id = 2,
         .offline_time = 1000,
