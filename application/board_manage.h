334caf7fd0befb10de7101638e2283337075e3b7
river <hello.river@outlook.com>
2026-05-19 11:03:58 +0800

    fix: Set ENCODER_FAKE_DATA_MODE to 0

--- application/board_manage.h
+++ application/board_manage.h
@@ -5,7 +5,7 @@
 #include "task.h"
 #include "offline_manage_task.h"
 
-#define ENCODER_FAKE_DATA_MODE 1
+#define ENCODER_FAKE_DATA_MODE 0
 
 void board_get_mac_address(uint8_t *mac);
 void board_setup_components(void);
